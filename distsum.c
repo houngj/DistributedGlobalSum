cf7044a9bc2237ed972eb6f40d74d6e71eafcadc
GIT Admin <houngj@students.wwu.edu>
2014-01-22 14:37:33 -0800

    git node num

---
+++ distsum.c
@@ -0,0 +1,54 @@
+/* Sudo code from book
+if (I'm the master core) {
+    sum = my_x;
+    for each core other than myself {
+       receive value from core;
+       sum += value;
+    }
+} else {
+    send my_x to the master;
+}
+*/ 
+
+#include <stdio.h>
+#include <string.h>
+
+#define maxdepth 3
+int NodeNum = 0;
+
+typedef struct{
+  char instruction[3];
+  instruction next = NULL;
+} value;
+
+typedef struct {
+  value instuction;
+} Node;
+
+void recieveValueFromCore(int n, char* me){
+
+}
+
+void sendValueFromCore(int n, char* my_x){
+
+}
+
+void main(int mainargc, char **mainargv){
+  NodeNum = atoi(mainargv[1]);
+  Node NodeArray[NodeNum];
+  int i;
+  int x;
+  int odd;
+  for(i = 0; i < NodeNum; i++){
+    x = i+1;
+    y = x+1;
+    odd = (i+1) mod 2;
+    if(odd == 1){
+      //retrieve value from y  
+    } 
+    else{
+      //send value to x
+    }
+  }
+  
+}
